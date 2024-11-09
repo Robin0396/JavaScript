19357af21e9dc242f39c2a5b988dc24c8cba76f4
Robin <robin@metropolia.fi>
2024-11-09 15:31:11 +0530

    Initial commit

---
+++ Module 1/Task 5/task5.js
@@ -0,0 +1,14 @@
+
+const year = prompt('Enter a year');
+
+let leap = 'is not';
+
+console.log(year % 4);
+
+if (year % 4 === 0) {
+  leap = 'is';
+}
+
+const result = `Year ${year} ${leap} a leap year`;
+
+console.log(result)
---
+++ Module 1/Task 7/task7.js
@@ -0,0 +1,13 @@
+
+const numRolls = prompt('Enter the number of dice rolls');
+
+let sum = 0;
+
+for (let i = 0; i < numRolls; i++) {
+
+  const roll = Math.floor(Math.random() * 6 ) + 1;
+  sum += roll
+
+  }
+
+console.log(`Number of dice rolls: ${sum}`)
